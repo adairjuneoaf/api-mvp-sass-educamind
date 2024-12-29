df67d32341b46074ea8141ec96ad490e25567dfd
Adair Juneo <adair_juneo@hotmail.com>
2024-12-28 23:20:53 -0300

    last route create about using openai to generate some things with ai.

---
+++ src/routes/create-with-ai.ts
@@ -0,0 +1,69 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+
+import { prisma } from '../db/prisma';
+import { openai } from '../lib/openai';
+
+export const createWithAi = async (app: FastifyInstance) => {
+  app.post('/video/:videoId/create', async (req, res) => {
+    const { body, params } = req;
+
+    const paramsSchema = z.object({
+      videoId: z.string().uuid(),
+    });
+
+    const bodySchema = z.object({
+      templatePrompt: z.string(),
+      temperature: z
+        .number({ message: 'Must be a Number.' })
+        .min(0, { message: 'Min value is 0.' })
+        .max(1, { message: 'Max value is 1.' })
+        .default(0.25)
+        .optional(),
+    });
+
+    const { videoId } = paramsSchema.parse(params);
+    const { temperature, templatePrompt } = bodySchema.parse(body);
+
+    if (!videoId) {
+      return res.status(400).send({ error: 'Please send a video id to create transcription.' });
+    }
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video.transcription) {
+      return res.status(400).send({ error: 'Video transcription was not generated yet.' });
+    }
+
+    const promptMessage = templatePrompt.replace('{transcription}', video.transcription);
+
+    const aiChatReturn = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        {
+          role: 'developer',
+          content:
+            'Você é um chatbot para ajudar pessoas na geração de textos baseados em transcrições de conteúdos sobre tecnologia e programação.',
+        },
+        {
+          role: 'developer',
+          content:
+            'Os conteúdos são gravados ou escritos por pessoas que entendem do assunto que está transcrito para que você possa utilizar.',
+        },
+        {
+          role: 'developer',
+          content:
+            'Sempre se comporte como você sendo especialista nos assuntos. Não diga em terceira pessoa, citando como autor, desenvolvedor, escritor, palestrante ou algo do gênero.',
+        },
+        { role: 'user', content: promptMessage },
+      ],
+    });
+
+    return res.status(200).send({ content: aiChatReturn });
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
+import { fastifyCors } from '@fastify/cors';
 import fastifyMultipart from '@fastify/multipart';
 import { fastify } from 'fastify';
 
 import { createTranscription } from './routes/create-transcription';
+import { createWithAi } from './routes/create-with-ai';
 import { getPrompts } from './routes/get-prompts';
 import { uploadPrompts } from './routes/upload-video';
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST'],
+});
+
 app.register(fastifyMultipart, {
   limits: {
     fileSize: 1048576 * 25, // 25MB máximo
@@ -16,6 +24,7 @@ app.register(fastifyMultipart, {
 app.register(getPrompts);
 app.register(uploadPrompts);
 app.register(createTranscription);
+app.register(createWithAi);
 
 app.listen({ port: 3333 }, (error) => {
   if (error) throw error;
